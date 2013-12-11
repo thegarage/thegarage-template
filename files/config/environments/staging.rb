de12e5e83763a2ba141a1def499c49ff83eede39
Ryan Sonnek <ryan@codecrate.com>
2013-12-11 16:56:44 +0100

    add ERB evaluation of remote templates

--- files/config/environments/staging.rb
+++ files/config/environments/staging.rb
@@ -2,5 +2,5 @@
 require Rails.root.join('config/environments/production')
 
 # customize and override production settings here
-#{app_name.camelize}::Application.configure do
+<%= app_name.camelize %>::Application.configure do
 end
